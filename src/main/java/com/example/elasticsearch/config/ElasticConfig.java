2ad4e371b6bd091f90a9fff012c058c3ec51d6e0
carlos <carlos@carlos>
2024-06-02 23:17:58 +0200

    Subida inicial

---
+++ src/main/java/com/example/elasticsearch/config/ElasticConfig.java
@@ -0,0 +1,36 @@
+package com.example.elasticsearch.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticConfig {
+
+	@Value("${elasticsearch.host}")
+	private String[] domains;
+
+	@Value("${elasticsearch.port}")
+	private Integer port;
+
+	@Value("${elasticsearch.protocol}")
+	private String protocol;
+
+	@Bean
+	public RestHighLevelClient elasticHighClient() {
+
+		HttpHost[] hosts = new HttpHost[domains.length];
+
+		for (int i = 0; i < domains.length; i++) {
+			hosts[i] = new HttpHost(domains[i], port, protocol);
+		}
+
+		return new RestHighLevelClient(RestClient
+				.builder(hosts).setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder
+						.setConnectTimeout(30000).setConnectionRequestTimeout(0).setSocketTimeout(120000))
+				.setMaxRetryTimeoutMillis(120000));
+	}
+}
